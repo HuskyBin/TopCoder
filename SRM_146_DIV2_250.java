c0cea297ce215ef38458d5cd3f25966db279ee07
HuskyBin <xiang.zh@husky.neu.edu>
2014-08-18 12:34:24 -0400

    Update SRM_146_DIV2_250.java

--- SRM_146_DIV2_250.java
+++ SRM_146_DIV2_250.java
@@ -1,3 +1,24 @@
+/*
+Problem Statement
+    
+This task is about the scoring in the first phase of the die-game Yahtzee, where five dice are used. The score is determined by the values on the upward die faces after a roll. The player gets to choose a value, and all dice that show the chosen value are considered active. The score is simply the sum of values on active dice.
+Say, for instance, that a player ends up with the die faces showing 2, 2, 3, 5 and 4. Choosing the value two makes the dice showing 2 active and yields a score of 2 + 2 = 4, while choosing 5 makes the one die showing 5 active, yielding a score of 5.
+Your method will take as input a int[] toss, where each element represents the upward face of a die, and return the maximum possible score with these values.
+Definition
+    
+Class:
+YahtzeeScore
+Method:
+maxPoints
+Parameters:
+int[]
+Returns:
+int
+Method signature:
+int maxPoints(int[] toss)
+(be sure your method is public)
+*/
+
 import java.util.Arrays;
 
 public class YahtzeeScore {
